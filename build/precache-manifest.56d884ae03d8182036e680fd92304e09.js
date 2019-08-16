da5b0354a13bc870565be4ffb1b321bff74974c2
MattG-ZA <gleedmatt@gmail.com>
2019-08-16 18:52:44 +0200

    Added github-pages deployments.

---
+++ build/precache-manifest.56d884ae03d8182036e680fd92304e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff541d6c7add29cdc6292db25a52e34c",
+    "url": "/mingalaxy/index.html"
+  },
+  {
+    "revision": "d421b52ff3951fa238ae",
+    "url": "/mingalaxy/static/css/main.0325f2fc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/mingalaxy/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d421b52ff3951fa238ae",
+    "url": "/mingalaxy/static/js/main.f86c6b72.chunk.js"
+  },
+  {
+    "revision": "ee6c6c529c688a4530e7",
+    "url": "/mingalaxy/static/js/runtime~main.b0771d9d.js"
+  }
+]);
