ce11747f9633c18a992f5f9dbc4d67e7f7796383
MattG-ZA <gleedmatt@gmail.com>
2019-08-18 22:02:15 +0200

    Updates

--- precache-manifest.8e06bb23a651d15801e69e898351225c.js
+++ precache-manifest.8e06bb23a651d15801e69e898351225c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4f6b6671a5811eda462192adc574d69",
+    "revision": "ca94d7898025a7055edea032c3621802",
     "url": "/mingalaxy/index.html"
   },
   {
-    "revision": "f5a206d588c0ef4338c4",
-    "url": "/mingalaxy/static/css/main.d8732db3.chunk.css"
+    "revision": "d50f99faa89627e2b00c",
+    "url": "/mingalaxy/static/css/main.d0556e4f.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/mingalaxy/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "f5a206d588c0ef4338c4",
-    "url": "/mingalaxy/static/js/main.990b57dd.chunk.js"
+    "revision": "d50f99faa89627e2b00c",
+    "url": "/mingalaxy/static/js/main.10582479.chunk.js"
   },
   {
     "revision": "ee6c6c529c688a4530e7",
